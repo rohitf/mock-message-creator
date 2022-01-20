3040459fab8b1ec0bb9c6e0a17e45f282cbc1f4b
Rohit Falor <rohitfalor@outlook.com>
2022-01-20 01:23:54 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Sveltekit Tailwind & Typescript Template
+
+This is a starter template to jumpstart your project with Typescript and the latest version of [Tailwindcss](https://tailwindcss.com/), with minimal to no configuration needed.
+
+## Installation
+
+Run this command in your terminal:
+
+```bash
+npx create-tailwindkit-ts@latest your-app-name-here
+```
+
+## Usage
+
+If you want to enable dark mode you will have to modify the tailwind.config.cjs file
+
+```javascript
+module.exports = {
+	purge: ['./src/**/*.svelte'],
+	darkMode: 'class', // or 'media'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/routes/index.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+	let otherPerson = "Saurabh Karmarkar";
+	$: initials = otherPerson
+		.split(" ")
+		.map((s) => s.charAt(0).toUpperCase())
+		.join("");
+
+	interface Message {
+		ownMessage: boolean;
+		content: string;
+	}
+
+	let messages: Message[] = [
+		{
+			ownMessage: true,
+			content: "Hey any tips for the exam?",
+		},
+		{
+			ownMessage: false,
+			content: "Do not flee in my car",
+		},
+		{
+			ownMessage: true,
+			content: "Umm...",
+		},
+	];
+
+	function addMessage() {
+		console.log("clicked dbl");
+		messages = [
+			...messages,
+			{
+				ownMessage: true,
+				content: "",
+			},
+		];
+	}
+
+	function deleteMessage(event: MouseEvent, index) {
+		event.preventDefault();
+		messages = [...messages.slice(0, index), ...messages.slice(index + 1)];
+	}
+</script>
+
+<body
+	class="w-screen h-screen absolute left-0 top-0 grid place-items-center text-center p-4"
+>
+	<main class="w-fit rounded-md shadow-md">
+		<section class="header flex justify-center w-96 p-4 rounded-t-md">
+			<i class="fas fa-comment-alt text-2xl" />
+			<div class="text-xl bg-transparent ml-3">{otherPerson}</div>
+		</section>
+		<section
+			class="flex flex-col h-[500px] bg-white rounded-b-md overflow-y-scroll pb-4"
+			on:dblclick={addMessage}
+		>
+			{#each messages as { ownMessage, content }, i}
+				{#if ownMessage}
+					<div class="flex mt-4 justify-end">
+						<div
+							contenteditable
+							on:contextmenu={(e) => deleteMessage(e, i)}
+							class="bg-gray-200 rounded-lg mr-2 p-3 text-left"
+						>
+							{content}
+						</div>
+						<div
+							class="avatar bg-blue-500 mr-4"
+							on:click={() => (ownMessage = !ownMessage)}
+						>
+							<i class="fas fa-user" />
+						</div>
+					</div>
+				{:else}
+					<div class="flex ml-4 mt-5">
+						<div
+							class="avatar bg-purple-500"
+							on:click={() => (ownMessage = !ownMessage)}
+						>
+							{initials}
+						</div>
+						<div
+							contenteditable
+							class="bg-gray-200 rounded-lg ml-2 p-3 text-left"
+						>
+							{content}
+						</div>
+					</div>
+				{/if}
+			{/each}
+		</section>
+	</main>
+	<h1 class="text-2xl text-gray-700">
+		<ul class="list-disc text-left">
+			<li>Double click screen to add message</li>
+			<li>Click name in header to edit</li>
+			<li>Click message to edit</li>
+			<li>Click sender icon to switch sender</li>
+			<li>Right click message to remove</li>
+			<li>Scroll to see more messages</li>
+		</ul>
+	</h1>
+</body>
+
+<style>
+	body {
+		background: linear-gradient(
+			270deg,
+			rgba(34, 193, 195, 1) 0%,
+			rgb(251, 203, 100) 100%
+		);
+	}
+
+	.header {
+		background: rgba(255, 255, 255, 0.6);
+		backdrop-filter: blur(10px);
+	}
+
+	.avatar {
+		@apply grid place-items-center text-white w-10 h-10 rounded-full cursor-pointer;
+	}
+</style>
